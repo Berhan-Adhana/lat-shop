2fc5312c003ccbff6c7c55380b44d02c11966962
Berhan-Adhana <berhanghe90@gmail.com>
2026-04-15 23:57:00 -0600

    role type changed

--- src/lib/auth/auth.options.ts
+++ src/lib/auth/auth.options.ts
@@ -78,7 +78,7 @@ export const authOptions: NextAuthOptions = {
     async session({ session, token }) {
       if (token) {
         session.user.id = token.id as string;
-        session.user.role = token.role as string;
+        session.user.role = token.role as "CUSTOMER" | "ADMIN";
       }
       return session;
     },
